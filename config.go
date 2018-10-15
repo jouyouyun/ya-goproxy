513c4c3af14f662a97436f5c227131e8220a59dc
jouyouyun <jouyouwen717@gmail.com>
2018-10-15 13:08:25 +0800

    feat: add pid file and signal handler

---
+++ config.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+config contains:
+    LocalAddr:
+    LocalPort:
+    ProxyAddr:
+    ProxyPort:
+    ProxyFile: the known domain list, will convert to ip in internal
+    ProxyCacheFile: the direct access failure ip list
+    ProxyCacheDuration:
+    DirectTimeout:
+    UDPEnabled:
+    UDPOverTCP:
+*/
--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net"
 	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
 	"time"
 )
 
 type Manager struct {
-	listener net.Listener
-	quit     bool
-	// quitLocker sync.Mutex
+	tcpListener net.Listener
+	udpListener net.Listener
 }
 
+const (
+	pidFile = "/tmp/ya-goproxy.pid"
+)
+
 var (
-	port = flag.String("p", "8081", "the listen port")
+	tcpAddress = flag.String("p", "127.0.0.1:8081", "the tcp listen address")
 
 	_m *Manager
 )
@@ -23,42 +32,74 @@ var (
 func main() {
 	flag.Parse()
 
-	if len(os.Args) == 2 && (os.Args[1] == "-h" ||
-		os.Args[1] == "--help") {
-		flag.Usage()
+	if checkExists() {
+		fmt.Println("There has a 'ya-goproxy' running...")
 		return
 	}
-
-	listener, err := net.Listen("tcp", "127.0.0.1:"+*port)
+	_m = new(Manager)
+	err := _m.init()
 	if err != nil {
-		fmt.Println("Failed to listen port:", err, *port)
+		fmt.Println("Failed to init:", err)
 		return
 	}
+	defer _m.finalize()
 
-	_m = &Manager{listener: listener, quit: false}
-	_m.Loop()
+	err = genPIDFile()
+	if err != nil {
+		fmt.Println("Failed to generate pid file:", err)
+		return
+	}
+	defer os.Remove(pidFile)
+
+	go _m.handleSignal()
+	_m.TCPLoop()
 }
 
-func (m *Manager) Loop() {
+func (m *Manager) init() error {
+	tcp, err := net.Listen("tcp", *tcpAddress)
+	if err != nil {
+		return err
+	}
+	m.tcpListener = tcp
+	return nil
+}
+
+func (m *Manager) finalize() {
+	if m.tcpListener != nil {
+		m.tcpListener.Close()
+		m.tcpListener = nil
+	}
+	if m.udpListener != nil {
+		m.udpListener.Close()
+		m.udpListener = nil
+	}
+}
+
+func (m *Manager) handleSignal() {
+	var c = make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-c
+	fmt.Println("Recieve signal:", sig.String())
+	_m.finalize()
+	os.Exit(-1)
+}
+
+func (m *Manager) TCPLoop() {
 	for {
-		if m.canQuit() {
-			fmt.Println("Quit...")
-			return
-		}
-		conn, err := m.accept()
+		conn, err := m.tcpAccept()
 		if err != nil {
 			fmt.Println("Failed to accept:", err)
 			return
 		}
-		go m.handle(conn)
+		go m.tcpHandle(conn)
 	}
 }
 
-func (m *Manager) accept() (net.Conn, error) {
+func (m *Manager) tcpAccept() (net.Conn, error) {
 	// if temporary error, continue
 	var tempDelay time.Duration
 	for {
-		conn, err := m.listener.Accept()
+		conn, err := m.tcpListener.Accept()
 		if err == nil {
 			tempDelay = 0
 			return conn, nil
@@ -80,26 +121,13 @@ func (m *Manager) accept() (net.Conn, error) {
 	}
 }
 
-func (m *Manager) handle(conn net.Conn) {
+func (m *Manager) tcpHandle(conn net.Conn) {
 	defer conn.Close()
-	connDump(conn)
-}
-
-func (m *Manager) canQuit() bool {
-	// m.quitLocker.Lock()
-	r := m.quit
-	// m.quitLocker.Unlock()
-	return r
-}
-
-func (m *Manager) doQuit() {
-	// m.quitLocker.Lock()
-	m.quit = true
-	// m.quitLocker.Unlock()
+	dumpTCPConn(conn)
 }
 
 // conn only contains data
-func connDump(conn net.Conn) {
+func dumpTCPConn(conn net.Conn) {
 	fmt.Println("Local Address:")
 	loc := conn.LocalAddr()
 	fmt.Println("\tNetwork:", loc.Network())
@@ -121,4 +149,49 @@ func connDump(conn net.Conn) {
 		fmt.Printf("\tRead %d Data []byte: %v\n", n, buf)
 		fmt.Printf("\tRead %d Data string: %v\n", n, string(buf))
 	}
+}
+
+func transport(rw1, rw2 io.ReadWriter) error {
+	errc := make(chan error, 1)
+	go func() {
+		_, err := io.Copy(rw1, rw2)
+		errc <- err
+	}()
+
+	go func() {
+		_, err := io.Copy(rw2, rw1)
+		errc <- err
+	}()
+
+	err := <-errc
+	if err != nil && err == io.EOF {
+		err = nil
+	}
+	return err
+}
+
+func genPIDFile() error {
+	pid := os.Getpid()
+	return ioutil.WriteFile(pidFile, []byte(fmt.Sprint(pid)), 0644)
+}
+
+func checkExists() bool {
+	contents, err := ioutil.ReadFile(pidFile)
+	if err != nil {
+		fmt.Println("Failed to read pid file:", err)
+		return false
+	}
+	pid, err := strconv.Atoi(string(contents))
+	if err != nil {
+		fmt.Println("Failed to convert string to int:", err)
+		return false
+	}
+
+	contents, err = ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
+	if err != nil {
+		fmt.Println("Failed to read pid cmdline:", err)
+		return false
+	}
+
+	return strings.Contains(string(contents), "ya-goproxy")
 }
